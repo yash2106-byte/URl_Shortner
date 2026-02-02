2ddb1f77155d16cc1e28a2686142418a2d5feab3
yash2106-byte <rajyash2510@gmail.com>
2026-02-02 23:26:18 +0530

    first commit before setting database

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ web.get('/',(req,res)=>{
     return res.json({status: 'server is up and running'})
 })
 web.listen(PORT, ()=>{console.log(`your server is up and running at ${PORT}`)})
+
+// docker run urlshortener drizzle-postgres -e POSTGRES_USER=yashraj POSTGRES_PASSWORD=yash2106 -d -p 5432:5432 postgres
