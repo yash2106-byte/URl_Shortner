56b5534f26f0ecab04729270407db845c686f056
yash2106-byte <rajyash2510@gmail.com>
2026-02-05 17:18:32 +0530

    pulled back all the unwanted change

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ web.get('/',(req,res)=>{
 })
 web.listen(PORT, ()=>{console.log(`your server is up and running at ${PORT}`)})
 
-// docker run urlshortener drizzle-postgres -e POSTGRES_USER=yashraj POSTGRES_PASSWORD=yash2106 -d -p 5432:5432 postgres
+
