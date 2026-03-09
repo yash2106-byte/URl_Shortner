a17628a5d85835060c175b3e763c7c4feb15e2d7
yash2106-byte <rajyash2510@gmail.com>
2026-03-10 02:54:50 +0530

    fixed index file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express'
 const web = express()
 
 const PORT  = process.env.PORT ?? 8000;
-
+web.use(express.json())
 web.get('/',(req,res)=>{
     return res.json({status : "Server is running"})
 })
