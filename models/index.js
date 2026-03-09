6deedb1251e5e268a8f1b3fe6e37afbee6c3ffaa
yash2106-byte <rajyash2510@gmail.com>
2026-03-10 02:42:44 +0530

    schema setup done

---
+++ models/index.js
@@ -0,0 +1,1 @@
+export { userTable } from './schema'
---
+++ models/schema.js
@@ -0,0 +1,14 @@
+import { integer, varchar, pgTable, uuid,timestamp,text } from "drizzle-orm/pg-core"
+
+export const userTable = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  firstname: varchar('first_name',{length:55}).notNull(),
+  lastname: varchar('last_name',{length:55}),
+  email: varchar({length:200}).notNull().unique(),
+  password: text().notNull(),
+  salt:text().notNull(),
+
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').$onUpdate(()=>new Date()),
+
+})
